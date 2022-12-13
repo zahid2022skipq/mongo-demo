5f12a3c8008ee1b4ea6cd6ba9a7c29d1d876deb6
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-13 11:20:40 +0500

    mongoose schema

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", true);
 mongoose
-  .connect("mongodb://locahost/playground")
+  .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to mongoDB"))
   .catch((error) => console.log("Error occured ", error));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
